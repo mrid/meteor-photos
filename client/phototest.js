2d25dc3a32819a917809023d48e99d8d1e1f8222
Mona Rideout <dwivedi80@gmail.com>
2013-09-19 22:54:48 -0700

    misc

--- client/phototest.js
+++ client/phototest.js
@@ -1,7 +1,7 @@
 Session.setDefault('urls', []);
 Session.setDefault('buffers', []);
 
-Template.uploader.images = function() {
+Template.uploader.thumbs = function() {
   var retval = [];
   Session.get('urls').forEach(function(url) {
     retval.push({url: url});
@@ -10,36 +10,47 @@ Template.uploader.images = function() {
 };
 
 Template.uploader.events({
-
-  'change #img-add': function(inputEvt) {
-
-    var urls = [];
-    var buffers = [];
+  'change #select-image': function(inputEvt) {
     var files = inputEvt.srcElement.files;
-    var numImages = files.length;
-    for (var i = 0; i < numImages; i++) {
-      var file = files[i];
-
-      var urlReader = new FileReader();
-      urlReader.onloadend = function(fileReaderEvt) {
-        urls.push(fileReaderEvt.target.result);
-        if (urls.length == numImages) {
-          var newval = Session.get('urls').concat(urls);
-          Session.set('urls', newval);
-          $('#img-add').val('');
-        }
-      };
-      urlReader.readAsDataURL(file);
-
-      var bufferReader = new FileReader();
-      bufferReader.onloadend = function(fileReaderEvt) {
-        buffers.push(fileReaderEvt.target.result);
-        if (buffers.length == numImages) {
-          var newval = Session.get('buffers').concat(buffers);
-          Session.set('buffers', newval);
-        }        
-      };
-      bufferReader.readAsArrayBuffer(file);
-    }
+    showThumbs(files);
+    loadBuffers(files);
+  },
+  'click #btn-send': function() {
+    console.info('todo');
+    Session.set('urls', []);
+    Session.set('buffers', []);
   }
 });
+
+var showThumbs = function(blobs) {
+  var urls = [];
+  var numImages = blobs.length;
+  for (var index = 0; index < numImages; index++) {
+    var urlReader = new FileReader();
+    urlReader.onloadend = function(fileReaderEvt) {
+      urls.push(fileReaderEvt.target.result);
+      if (urls.length == numImages) {
+        var newval = Session.get('urls').concat(urls);
+        Session.set('urls', newval);
+        $('#img-add').val('');
+      }
+    };
+    urlReader.readAsDataURL(blobs[index]);
+  }
+};
+
+var loadBuffers = function(blobs) {
+  var buffers = [];
+  var numImages = blobs.length;
+  for (var index = 0; index < numImages; index++) {
+    var bufferReader = new FileReader();
+    bufferReader.onloadend = function(fileReaderEvt) {
+      buffers.push(fileReaderEvt.target.result);
+      if (buffers.length == numImages) {
+        var newval = Session.get('buffers').concat(buffers);
+        Session.set('buffers', newval);
+      }        
+    };
+    bufferReader.readAsArrayBuffer(blobs[index]);
+  }
+};
